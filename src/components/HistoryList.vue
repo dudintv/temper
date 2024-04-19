bba7ed8e29e1d5d904ebea0b6a3845bcbdd731fb
Dmitry Dudin <dudin.tv@gmail.com>
2024-04-19 15:48:28 +0200

    feat: consider empty states

--- src/components/HistoryList.vue
+++ src/components/HistoryList.vue
@@ -13,11 +13,17 @@ function stepTo(stepId: number) {
   <div class="bg-white shadow-md rounded-md overflow-clip">
     <h1 class="px-4 py-6 text-2xl leading-tight">List of actions committed</h1>
     <div class="p-4 bg-back">
-      <ul class="flex flex-col bg-white/50 shadow-md hover:shadow-lg rounded overflow-clip transition-all">
+      <ul
+        v-if="items?.length"
+        class="flex flex-col bg-white/50 shadow-md hover:shadow-lg rounded overflow-clip transition-all"
+      >
         <li v-for="step in items" :key="step.id" class="border-slate-200 border-b last:border-b-0 hover:bg-white">
           <HistoryListItem :item="step" @stepTo="stepTo(step.id)" />
         </li>
       </ul>
+      <div v-else>
+        <span class="text-primary/40">No actions</span>
+      </div>
     </div>
   </div>
 </template>
--- src/components/SortableList.vue
+++ src/components/SortableList.vue
@@ -8,11 +8,14 @@ defineProps<{ items: Post[] }>();
 <template>
   <div class="w-full">
     <h1 class="text-2xl mb-4 text-white leading-tight">Sortable Post List</h1>
-    <ul class="flex flex-col gap-4 w-full items-stretch">
+    <ul v-if="items?.length" class="flex flex-col gap-4 w-full items-stretch">
       <li v-for="(post, index) in items" :key="post.id" class="bg-back rounded">
         <SortableListItem :item="post" :is-first="index === 0" :is-last="index === items.length - 1" />
       </li>
     </ul>
+    <div v-else>
+      <span class="text-white/40">No posts</span>
+    </div>
   </div>
 </template>
 
