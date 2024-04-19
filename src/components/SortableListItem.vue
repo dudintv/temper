7975017e17abaec1450d523728a28fef080759da
Dmitry Dudin <dudin.tv@gmail.com>
2024-04-19 17:18:06 +0200

    chore: small UI improvements

--- src/components/SortableListItem.vue
+++ src/components/SortableListItem.vue
@@ -8,7 +8,10 @@ const emit = defineEmits(["up", "down"]);
 
 <template>
   <div class="bg-white/50 hover:bg-white flex justify-between rounded shadow-md hover:shadow-lg transition-all">
-    <div class="flex items-center p-4">Post {{ item.id }}</div>
+    <div class="flex items-center p-4 gap-4">
+      <span class="flex-shrink-0">Post {{ item.id }}</span>
+      <span class="text-slate-300">{{ item.title }}</span>
+    </div>
     <SortableButtons :hide-up="isFirst" :hide-down="isLast" @up="emit('up')" @down="emit('down')" />
   </div>
 </template>
